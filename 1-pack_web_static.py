8bf1eaf51e079f55b59a2f6feedc0144a8cfd83f
wang-arshton256 <wangarshton@gmail.com>
2024-03-07 15:11:51 -0800

    Compress before sending

---
+++ 1-pack_web_static.py
@@ -0,0 +1,19 @@
+i#!/usr/bin/python3
+# Fabscript creat .tgz archive contents of web_static.
+
+from datetime import datetime
+from fabric.api import local
+from os.path import isdir
+
+
+def do_pack():
+    """generates a tgz archive"""
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except:
+        return None
